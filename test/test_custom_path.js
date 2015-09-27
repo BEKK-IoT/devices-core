ebcd6da4df804edc78e285c9d752318778a26aa0
Vegard Veiset <veiset@gmail.com>
2015-09-27 13:25:28 +0200

    Code-style.

--- test/test_custom_path.js
+++ test/test_custom_path.js
@@ -1,18 +1,18 @@
-// Fail test if it takes more than 5000ms
-setTimeout(() => process.exit(1), 5000);
-
 import Firebase from '../lib/firebase';
+const [SUCCESS, ERROR] = [0, 1];
 const TEAM = 'TEST_DO_NOT_UPVOTE';
 const fb = new Firebase(TEAM, 'TEST/');
 
+setTimeout(() => process.exit(ERROR), 5000);
+
 fb.on('test', `/users/${TEAM}`, function(value) {
 	console.log('Got unexpected event: ', value);
-	process.exit(1);
+	process.exit(ERROR);
 });
 
 fb.on('test', `/TEST/${TEAM}`, function(value) {
 	console.log('Got response: ', value);
-	fb.defaultConnection.remove((error) => error ? process.exit(1) : process.exit(0));
+	fb.defaultConnection.remove((error) => error ? process.exit(ERROR) : process.exit(SUCCESS));
 });
 
 fb.send('test', 'test');
--- test/test_default_send.js
+++ test/test_default_send.js
@@ -1,13 +1,12 @@
-// Fail test if it takes more than 5000ms
-setTimeout(() => process.exit(1), 5000);
-
 import Firebase from '../lib/firebase';
 const TEAM = 'TEST_DO_NOT_UPVOTE';
 const fb = new Firebase(TEAM);
+const [SUCCESS, ERROR] = [0, 1];
+setTimeout(() => process.exit(ERROR), 5000);
 
 fb.on('test', `/users/${TEAM}`, function(value) {
 	console.log('Got response: ', value);
-	fb.defaultConnection.remove((error) => error ? process.exit(1) : process.exit(0));
+	fb.defaultConnection.remove((error) => error ? process.exit(ERROR) : process.exit(SUCCESS));
 });
 
 fb.send('test', 'test');
