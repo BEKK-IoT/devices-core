5eb21b5f0ae2947597f5f728b94ce3a82f0b4eb2
alexander-svendsen <alexander.svendsen@bekk.no>
2015-09-27 16:17:45 +0200

    Repl default fault in all cases of object

--- dist/lib/johnny-five.js
+++ dist/lib/johnny-five.js
@@ -5,8 +5,8 @@ var five = require('johnny-five');
 //should by default not start the repl
 var _board = five.Board;
 five.Board = function () {
-  var opt = arguments.length <= 0 || arguments[0] === undefined ? { repl: false } : arguments[0];
-  return _board(opt);
+  var opt = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
+  return _board(Object.assign(opt, { repl: false }));
 };
 
 module.exports = five;
