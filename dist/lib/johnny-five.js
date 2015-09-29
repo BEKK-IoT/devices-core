1496d28f0228a351662d81deb9058a89cc83670a
Vegard Veiset <veiset@gmail.com>
2015-09-29 19:29:17 +0200

    Add workaround for voodoospark or johnny-five sensor init bug. Not sure which framework is stupid.

--- dist/lib/johnny-five.js
+++ dist/lib/johnny-five.js
@@ -4,9 +4,15 @@ var five = require('johnny-five');
 
 //should by default not start the repl
 var _board = five.Board;
-five.Board = function () {
-  var opt = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
-  return _board(Object.assign(opt, { repl: false }));
+five.Board = function (opt) {
+    console.log(_board);
+    _board = _board(Object.assign({ repl: false }, opt));
+    if (opt.hasOwnProperty('io')) {
+        _board.on('ready', function () {
+            return new five.Sensor("A5");
+        });
+    }
+    return _board;
 };
 
 module.exports = five;
