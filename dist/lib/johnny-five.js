508b57903726fb8ebd8c4d3d6d33afd682e645af
alexander-svendsen <alexander.svendsen@bekk.no>
2015-09-30 20:34:22 +0200

    Check if option exists before use in five

--- dist/lib/johnny-five.js
+++ dist/lib/johnny-five.js
@@ -5,9 +5,8 @@ var five = require('johnny-five');
 //should by default not start the repl
 var _board = five.Board;
 five.Board = function (opt) {
-    console.log(_board);
     _board = _board(Object.assign({ repl: false }, opt));
-    if (opt.hasOwnProperty('io')) {
+    if (opt && opt.hasOwnProperty('io')) {
         _board.on('ready', function () {
             return new five.Sensor("A5");
         });
